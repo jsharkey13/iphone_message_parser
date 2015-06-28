8bae0fd0f01e37c3e6fa10814f0a509d14e39c90
James Sharkey <jsharkey13@users.noreply.github.com>
2015-06-28 17:35:33 +0100

    Add code to run from the command line, taking an optional filename argument. Add docstrings along lines of PEP257.

--- ios_parser.py
+++ ios_parser.py
@@ -6,6 +6,17 @@ import ios_chat
 
 
 class iOSMessageParse(object):
+    """An object to encapsulate all the methods required to parse the iOS6 sms.db.
+
+       These include methods to initialise, save and load a ios_chat.Chat object,
+       which contains a Pythonic representation of iPhone message history.
+        - Can read in messages from the sms.db sqlite database.
+        - Can dump the Chat object to a pickle file and load it again in another
+          session: use dump_to_pickle() and load_from_pickle().
+        - Can export messages to csv format: use write_to_csv()
+        - Using a 'mobilenumbers_people' file, can turn unrecognised +NNNNNNNNNNN
+          phone numbers into names. Lines should be '[mobile_number]:[name]'.
+          See the print_unknowns() function."""
 
     _MYNAME = "My Name"
     _MYNUMBER = "+NNNNNNNNNNN"
@@ -41,25 +52,36 @@ class iOSMessageParse(object):
             self.load_from_pickle(fname)
 
     def _close(self):
+        """Close any open database and cursor before deletion. Do not call manually."""
         if self._sqlite_db is not None:
             self._cursor = None
             self._sqlite_db.close()  # Close connection without committing; don't save changes
 
     def __del__(self):
+        """Ensure _close() is called on deletion."""
         self._close()
-    
+
     def _verify_file(self):
-        # Verify that we're parsing an sms.db file:
+        """Verify the file loaded is an sms.db file.
+
+           It must contain a 'handle' table and a 'message' table; if these are
+           not both present; the file might be an sqlite databse, but is not the
+           iOS6 sms.db file. Will abort whole program execution if fails!"""
         _correct_tables = 0
         self._cursor.execute("SELECT * FROM sqlite_master WHERE name='handle' AND type='table'")
         _correct_tables += len(self._cursor.fetchall()) # Must have 'handle' table (add 1 if does, 0 if not)
         self._cursor.execute("SELECT * FROM sqlite_master WHERE name='message' AND type='table'")
-        _correct_tables += len(self._cursor.fetchall()) # And 'message' table (add 1 if does, 0 if not) 
+        _correct_tables += len(self._cursor.fetchall()) # And 'message' table (add 1 if does, 0 if not)
         if _correct_tables != 2:
             print "The database does not contain a handle table and a message table. Fatal Error: Abort."
             sys.exit(-1)
 
     def _read_handles(self):
+        """Read in the 'handle' table and add line entries to dictionaries.
+
+           Called automatically; do not call manually. Read in the 'handle'
+           table from the database and add line entries to the dictionaries used
+           to translate between iOS contact handle and Name, and vice versa."""
         if self._cursor is None:
             print "Cannot read handles from database; was data loaded from a pickle file?"
             return
@@ -69,6 +91,14 @@ class iOSMessageParse(object):
             self._HANDLENUMBER.update({handle[0]: handle[1]})
 
     def _read_numbers_people(self):
+        """Read in the 'mobilenumbers_people' file and add line entries to dictionaries.
+
+           Called automatically; do not call manually. Read in the 'mobilenumbers_people'
+           file and add line entries to the dictionaries used to translate between
+           phone number and Name, and vice versa.
+            - Lines should be formatted '[mobile_number]:[name]'.
+            - Ill-formatted lines are ignored, and the file does not have to be present
+              for the code to function: unrecognised numbers are left unchanged."""
         try:
             with open('mobilenumbers_people') as f:
                 lines = [line.rstrip('\n') for line in f]
@@ -82,9 +112,17 @@ class iOSMessageParse(object):
             pass
 
     def _parse_message_num(self, num):
+        """Turn string form of message number into an integer."""
         return int(num)
 
     def _parse_message_handle(self, handle):
+        """Tidy up the name of the sender of a message.
+
+           The message handle is turned into a mobile number using the _HANDLENUMBER
+           dictionary. If the number is recognised, use the _NUMBERNAME dictionary
+           to replace this with a name if possible. Any numbers which remain unchanged
+           are added to a list to facilitate populating a 'mobilenumbers_people'
+           file: see print_unknowns()."""
         try:
             num = self._HANDLENUMBER[handle]
         except KeyError, error:
@@ -103,6 +141,7 @@ class iOSMessageParse(object):
         return name
 
     def _parse_message_author(self, name, is_from_me):
+        """Return the name of message author, given a boolean is_from_me."""
         is_from_me = bool(is_from_me)
         if is_from_me:
             author = self._MYNAME
@@ -111,11 +150,22 @@ class iOSMessageParse(object):
         return author
 
     def _parse_message_date(self, mac_time):
+        """Turn the datestamp on the message into a datetime object.
+
+           The datestamp is stored in Mac Time; add 978307200 seconds to make
+           it into UNIX time and then convert UNIX time into a datetime.datetime
+           object."""
         unix_time = int(mac_time) + 978307200
         date = datetime.datetime.fromtimestamp(unix_time)
         return date
 
     def _parse_message_body(self, message_body):
+        """Tidy up the message body itself.
+
+           This turns newline characters into a unique custom string which can
+           be replaced after export if necessary. Quote marks are also escaped,
+           to allow the use of quotes and commas in messages whilst allowing
+           export to csv. Those two lines can be removed if desired."""
         if message_body is None:
             message_body = ""
         message_body = '<|NEWLINE|>'.join(message_body.splitlines())  # We can't have newline characters in a csv file
@@ -123,18 +173,27 @@ class iOSMessageParse(object):
         return message_body
 
     def print_unknowns(self):
+        """Print out any mobile numbers not recognised by the code.
+
+           Prints lines containing unrecognised numbers, along with instructions on
+           how to add the names to the 'mobilenumbers_people' file."""
         if self.Texts is None:
             print "The database file has not been parsed. Run parse_messages()."
             return
         if len(self._UNKNOWNS) == 0:
             return
         self._UNKNOWNS = list(set(self._UNKNOWNS))  # An unordered duplicate removal method
-        print "After identifying any of these accounts, add '[mobilenumber]:[name]' to 'mobilenumbers_people'"
+        print "After identifying any of these accounts, add '[mobile_number]:[name]' to a file in the current directory named 'mobilenumbers_people'"
         print "Email addresses for iMessage contacts may appear, as may company names: treat these as mobile numbers."
         for uid in self._UNKNOWNS:
             print uid
 
     def parse_messages(self):
+        """Take the loaded database and create a Chat object.
+
+           Takes the sms.db file and reads in the messages using SQLite. Creates
+           the Chat object, which can be used independently and accessed as the
+           iOSMessageParse.Texts object."""
         if self._sqlite_db is None:
             print "No database open. Was data loaded from a pickle file?"
             return
@@ -175,6 +234,11 @@ class iOSMessageParse(object):
         self.Texts = ios_chat.Chat(self._MYNAME, _chat_list)
 
     def write_to_csv(self, filename='sms.csv', chronological=False):
+        """Export all messages to csv format.
+
+           The filename can be specified as an optional argument. If 'chronological'
+           is True, messages are printed in date order, otherwise they are printed
+           grouped in Threads sorted by total thread length."""
         with open(filename, "w") as f:
             header_line = '"Thread","Message Number","Message Author","Message Timestamp","Message Body"\n'
             f.write(header_line.encode('utf8'))
@@ -189,10 +253,21 @@ class iOSMessageParse(object):
                         f.write(text.encode('utf8'))
 
     def dump_to_pickle(self, filename='sms.pickle'):
+        """Serialise the Chat object to a pickle file.
+
+           The pickle file can be used to restore the Chat object in another
+           session without re-importing the zip or htm file. Load either using
+           load_from_pickle(), or in another program using Pickle's standard load()
+           command."""
         with open(filename, "w") as f:
             pickle.dump(self.Texts, f)
 
     def load_from_pickle(self, filename='sms.pickle'):
+        """Read in the pickle file, optionally from a specified filename.
+
+           The function sets the internal Chat object and returns the Chat object.
+           Provided mainly as an example, since the parser's main aim is to read
+           in from the sms.db file, and to output csv or the Chat object."""
         with open(filename, "r") as f:
             self.Texts = pickle.load(f)
         return self.Texts
--- sms.py
+++ sms.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import codecs
 
 import ios_parser
@@ -13,6 +14,34 @@ streamWriter = codecs.lookup('utf-8')[-1]
 sys.stdout = streamWriter(sys.stdout)
 
 if __name__ == "__main__":
-    SMS = ios_parser.iOSMessageParse('sms.db')
-    SMS.parse_messages()
+    """Allow the parser to be run from the command line.
+
+       Optionally, the function allows specifying the filename to read in from
+       as the first argument."""
+    if len(sys.argv) >= 2:
+        # If filname passed in and a recognised format, continue:
+        if ((".db" in sys.argv[1]) or (".sqlite" in sys.argv[1]) or (".pickle" in sys.argv[1])):
+            fname = sys.argv[1]
+        else:
+            # If not a recognised format, stop but allow override:
+            print "File is not a .db file, a .sqlite file or a pickle file."
+            print "Later code will verify the file is an sms.db file, and will terminate anyway if not."
+            cont = raw_input("Continue now? (y/n)")
+            if cont == "n":
+                sys.exit(-1)
+    else:
+        # If no argument, attempt to open the default sms.db file:
+        fname = "sms.db"
+    if not os.path.isfile(fname):
+        print "File " + fname + " does not exist or could not be found! Abort."
+        sys.exit(-1)
+
+    # Some example code to add functionality immediately.
+
+    # Create the parser, and parse the messages file:
+    if ".pickle" in fname:
+        SMS = ios_parser.iOSMessageParse(fname, load_pickle=True)
+    else:
+        SMS = ios_parser.iOSMessageParse(fname)
+        SMS.parse_messages()
     print SMS.Texts
